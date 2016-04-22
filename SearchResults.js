3f89f36ed74b3c675844d323bfce176c16750e8d
kat markava <katryc@outlook.com>
2016-04-21 23:30:25 -0400

    added styling for rows

--- SearchResults.js
+++ SearchResults.js
@@ -10,6 +10,33 @@ var {
   Text,
   Component
 } = React;
+var styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+});
 
 class SearchResults extends Component {
 
